e08201ce1485ba5fd9bc3e15be4c8cba73140185
Alexey Rusel <alexeyrusel@gmail.com>
2023-01-06 23:09:22 +0300

    feat: add modal section
    also add basic layout for all pages

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,27 @@
 let [fileName] = location.pathname.split("/").slice(-1);
-let footerNavbarLinks = document.querySelectorAll('.footer-navbar__link');
-let modalLinks = document.querySelectorAll('.modal__link');
+let footerNavbarLinks = document.querySelectorAll(".footer-navbar__link");
+let modalLinks = document.querySelectorAll(".modal__link");
 
 footerNavbarLinks.forEach((link) => {
-    if(link.getAttribute('href') === fileName) {
-        link.classList.add('footer-navbar__link_active-page');
+    if (link.getAttribute("href") === fileName) {
+        link.classList.add("footer-navbar__link_active-page");
     }
 });
 
 modalLinks.forEach((link) => {
-    if(link.getAttribute('href') === fileName) {
-        link.classList.add('modal__link_active-page');
+    if (link.getAttribute("href") === fileName) {
+        link.classList.add("modal__link_active-page");
     }
+});
+
+let openModalButton = document.querySelector(".open-modal-button");
+let closeModalButton = document.querySelector(".close-modal-button");
+let modal = document.querySelector(".modal");
+
+openModalButton.addEventListener("click", () => {
+    modal.classList.add("modal_open");
+});
+
+closeModalButton.addEventListener("click", () => {
+    modal.classList.remove("modal_open");
 });
